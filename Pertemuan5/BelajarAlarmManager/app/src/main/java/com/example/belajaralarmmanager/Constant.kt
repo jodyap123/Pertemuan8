7c7923eb715e4246231f83deb4191ad282b60cd1
jodyap123 <jody18ti@mahasiswa.pcr.ac.id>
2020-12-06 18:41:01 +0700

    Commit Ketiga

--- Pertemuan5/BelajarAlarmManager/app/src/main/java/com/example/belajaralarmmanager/Constant.kt
+++
@@ -1,7 +0,0 @@
-package com.example.belajaralarmmanager
-
-object Constants {
-    const val EXTRA_EXACT_ALARM_TIME = "EXTRA_EXACT_ALARM_TIME"
-    const val ACTION_SET_EXACT = "ACTION_SET_EXACT"
-    const val ACTION_SET_REPETITIVE_EXACT = "ACTION_SET_REPETITIVE_EXACT"
-}
--- Pertemuan5/BelajarAlarmManager/app/src/main/java/com/example/belajaralarmmanager/RandomIntUtil.kt
+++
@@ -1,8 +0,0 @@
-package com.example.belajaralarmmanager
-
-import java.util.concurrent.atomic.AtomicInteger
-
-object RandomUtil {
-    private val seed = AtomicInteger()
-    fun getRandomInt() = seed.getAndIncrement() + System.currentTimeMillis().toInt()
-}
